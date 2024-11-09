de4c4499042052553e4262c4c398e55543cea854
PavanKumar1119 <pbmpavankumar11175@gmail.com>
2024-11-09 17:20:01 +0530

    First Commit

---
+++ BACKEND/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/mongodb.js";
+import dotenv from "dotenv";
+import { connectCloudinary } from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+dotenv.config();
+
+//App config
+const app = express();
+const PORT = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+//api endpoints
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.listen(PORT, () => {
+  console.log("SERVER STARTED ON PORT " + PORT);
+});
